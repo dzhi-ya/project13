8de8169c350d168984016abcae1a68262d9b7c95
Daria <daria_zhitnaya@inbox.ru>
2021-01-16 00:00:20 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import {
+  MyWorker,
+  MyWorkersDatabase,
+  MyWorkerType,
+} from './shared/worker.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'project13';
+  title = 'Список сотрудников';
+  workers: MyWorker[] = MyWorkersDatabase;
+  myWorkerType = MyWorkerType;
+  workerEdit: any;
+  id: any;
+  name: any;
+  surname: any;
+  phone: any
+  type: any;
+  modal = false;
+
+  // mask: Array<string | RegExp>;
+
+  // editForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   surname: new FormControl(''),
+  //   phone: new FormControl(''),
+  //   id: new FormControl(''),
+  // });
+
+  editForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    surname: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    type: new FormControl('', [Validators.required]),
+    id: new FormControl(''),
+  });
+
+  public mask = ['+', 7, ' ', '(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/];
+
+
+
+  getByType(type: number) {
+    return this.workers.filter((worker) => worker.type === type);
+  }
+
+  onDeleteById(id: number) {
+    let index = this.workers.findIndex((worker) => worker.id === id);
+    if (index !== -1) {
+      this.workers.splice(index, 1);
+    }
+  }
+
+  onAddWorker(worker) {
+    let id =
+      this.workers.length > 0
+        ? this.workers[this.workers.length - 1].id + 1
+        : 1;
+    worker.id = id;
+    this.workers.push(worker);
+  }
+
+  onEditById(worker){
+    this.workerEdit = worker;
+    this.modal = !this.modal;
+    this.id = this.workerEdit.id;
+    this.name = this.workerEdit.name;
+    this.surname = this.workerEdit.surname;
+    this.phone = this.workerEdit.phone;
+    this.type = this.workerEdit.type;
+  }
+  
+  onEditData(worker){
+    this.workers[this.workerEdit.id - 1].name = this.name;
+    this.workers[this.workerEdit.id - 1].surname = this.surname;
+    this.workers[this.workerEdit.id - 1].phone = this.phone;
+    this.workers[this.workerEdit.id - 1].type = this.type;
+    this.modal = !this.modal;
+  }
+
+  onClose(): void{
+    this.modal = !this.modal;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TextMaskModule } from 'angular2-text-mask';
 
 import { AppComponent } from './app.component';
+import { TableWorkersComponent } from './ui/table-workers/table-workers.component';
+import { AddformWorkerComponent } from './ui/addform-worker/addform-worker.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TableWorkersComponent, AddformWorkerComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // NgbModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    TextMaskModule,
+    // MatProgressSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
